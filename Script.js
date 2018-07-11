41cbbc21c6405a62e79935593cfd73aba618a958
Nattapon Benz <sevipsagis@gmail.com>
2018-07-11 14:31:12 +0700

    fix play after game end

--- Script.js
+++ Script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     var gameBox = $(".gameBox");
 
     gameBox.on("mouseover", function () {
-        if (this.innerHTML == "") {
+        if (this.innerHTML == "" && gameStatus == false) {
             $(this).addClass("hover-" + thisTurn);
         }
     });
@@ -21,39 +21,41 @@ $(document).ready(function () {
         $(this).removeClass("hover-" + thisTurn);
     });
     gameBox.on("click", function () {
-        if (gameStatus == false && !($(this).hasClass("has-O")) && !($(this).hasClass("has-X"))){
+        if (gameStatus == false && !($(this).hasClass("has-O")) && !($(this).hasClass("has-X"))) {
             $(this).append(thisTurn);
             $(this).addClass("has-" + thisTurn);
             $(this).removeClass("hover-O hover-X");
-            // changeTurn
-            if(thisTurn == "O"){thisTurn = "X";}
-            else{thisTurn = "O";}
-            gameTurn++;
-            if(gameTurn >= 5){
+            if (gameTurn >= 5) {
                 WinConditionCheck();
-            } 
+            }
+            // changeTurn
+            if (thisTurn == "O") { thisTurn = "X"; }
+            else { thisTurn = "O"; }
+            gameTurn++;
         }
     });
 
-    function WinConditionCheck(){
+    function WinConditionCheck() {
         // Create Array
         var arr = [];
-        for(let arg of gameBox){
+        for (let arg of gameBox) {
             // push arg into arr
             arr.push(arg.innerHTML);
         }
         // assign value in arr to variable
         var [a0, a1, a2, b0, b1, b2, c0, c1, c2] = arr;
         // check condition
-        if(a0 == a1 && a1 == a2 && a0 != "" ||
-           b0 == b1 && b1 == b2 && b0 != "" ||
-           c0 == c1 && c1 == c2 && c0 != "" ||
-           a0 == b0 && b0 == c0 && a0 != "" ||
-           a1 == b1 && b1 == c1 && a1 != "" ||
-           a2 == b2 && b2 == c2 && a2 != "" ||
-           a0 == b1 && b1 == c2 && a0 != "" ||
-           a2 == b1 && b1 == c0 && a2 != "") {
-            alert(thisTurn);
+        if (a0 == a1 && a1 == a2 && a0 != "" ||
+            b0 == b1 && b1 == b2 && b0 != "" ||
+            c0 == c1 && c1 == c2 && c0 != "" ||
+            a0 == b0 && b0 == c0 && a0 != "" ||
+            a1 == b1 && b1 == c1 && a1 != "" ||
+            a2 == b2 && b2 == c2 && a2 != "" ||
+            a0 == b1 && b1 == c2 && a0 != "" ||
+            a2 == b1 && b1 == c0 && a2 != "") {
+            gameStatus = true;
+            Winner = thisTurn;
+            alert(Winner);
 
         }
     }
