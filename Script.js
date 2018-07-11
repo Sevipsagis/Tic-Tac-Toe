45062f4c719e2b4ef2ab57b3072eea11600bcb5d
Nattapon Benz <sevipsagis@gmail.com>
2018-07-11 13:15:47 +0700

    add hover effect

--- Script.js
+++ Script.js
@@ -1,9 +1,27 @@
 var resetButton = $("#resetButton");
-function reloadPage(){
-    alert("Test");
+var thisTurn = "O"
+var Winner = "--"
+var gameStatus = false;
+// Restart Game Function
+function reloadPage() {
     location.reload();
 }
-$(document).ready(function(){
+// Main
+$(document).ready(function () {
+    var title = gameTitle;
+    var gameBox = $(".gameBox");
 
-    
+    gameBox.on("mouseover", function () {
+        if (this.innerHTML == "") {
+            $(this).addClass("hover-" + thisTurn);
+        }
+    });
+    gameBox.on("mouseout", function () {
+        $(this).removeClass("hover-" + thisTurn);
+    });
+    gameBox.on("click", function () {
+        
+    });
+
+
 });
